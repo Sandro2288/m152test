f4b2321277a33a5cee0c6a8ff9c136be8a37624a
Sandro <sandro@go-solution.ch>
2019-01-09 11:42:10 +0100

    change name

--- docs/main.js
+++ docs/main.js
@@ -1275,7 +1275,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/sandro/go-dev/m152/first-app-cont/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /Users/sandro/go-dev/m152/m152test/src/main.ts */"./src/main.ts");
 
 
 /***/ })
--- docs/styles.js
+++ docs/styles.js
@@ -580,8 +580,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/sandro/go-dev/m152/first-app-cont/node_modules/@angular/material/prebuilt-themes/indigo-pink.css */"./node_modules/@angular/material/prebuilt-themes/indigo-pink.css");
-module.exports = __webpack_require__(/*! /Users/sandro/go-dev/m152/first-app-cont/src/styles.css */"./src/styles.css");
+__webpack_require__(/*! /Users/sandro/go-dev/m152/m152test/node_modules/@angular/material/prebuilt-themes/indigo-pink.css */"./node_modules/@angular/material/prebuilt-themes/indigo-pink.css");
+module.exports = __webpack_require__(/*! /Users/sandro/go-dev/m152/m152test/src/styles.css */"./src/styles.css");
 
 
 /***/ })
